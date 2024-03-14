aab9d86b4b50fa9c547ec28113af50836fa8699f
BR-BelalRezk <belalrezk.developer@gmail.com>
2024-03-14 03:40:01 +0200

    Update ProjectCard.tsx

--- src/components/content/ProjectCard.tsx
+++ src/components/content/ProjectCard.tsx
@@ -13,7 +13,7 @@ export default function ProjectCard({
       className="min-w-60 min-h-60 border-2 border-gray-500 rounded-lg flex flex-col items-center justify-center gap-5"
     >
       <h3 className="text-gray-300 font-semibold text-2xl">{project.label}</h3>
-      <h4 className="text-xs text-center px-1 text-gray-200 font-medium">
+      <h4 className="text-xs text-center px-5 text-gray-200 font-medium">
         {project.desc}
       </h4>
       <ul className="flex items-center justify-center gap-5 text-2xl text-gray-100">
