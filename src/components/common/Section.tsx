3a23cab68595c07bd7cb4ea5414daaa8cae463d7
BR-BelalRezk <belalrezk.developer@gmail.com>
2024-03-18 16:07:15 +0200

    u

--- src/components/common/Section.tsx
+++ src/components/common/Section.tsx
@@ -16,11 +16,11 @@ export default function Section({
   return (
     <section
       id={id}
-      className={`snap-center container mx-auto px-3 sm:px-5 md:px-10 lg:px-14 xl:px-16 2xl:px-20 h-dvh flex items-center justify-center
-      ${id === "fr" || id === "skills" ? "bg-black" : "bg-stone-950"}
-      `}
+      className={id === "fr" || id === "skills" ? "bg-stone-950" : "bg-black"}
     >
-      {children}
+      <article className="snap-center container mx-auto px-3 sm:px-5 md:px-10 lg:px-14 xl:px-16 2xl:px-20 h-dvh flex items-center justify-center">
+        {children}
+      </article>
     </section>
   );
 }
