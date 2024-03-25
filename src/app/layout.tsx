9bd244dc9f78bf53d8f8a44806bb3ec72e0d636a
BR-BelalRezk <belalrezk.developer@gmail.com>
2024-03-25 16:13:56 +0200

    u

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
 import Header from "@/components/layout/Header";
-import Stars from "@/components/layout/Stars";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +21,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="snap-y snap-mandatory !scroll-smooth">
       <body className={`${inter.className} bg-black text-gray-50`}>
-        <Stars />
         <Header />
         <main>{children}</main>
       </body>
